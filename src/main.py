21c173fb58fcabe322e7f5dbea852f73763f398b
Aleksander <65820898+rayydev@users.noreply.github.com>
2021-11-03 15:39:50 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+import colorama
+import psutil
+import platform
+import socket
+
+uname = platform.uname()
+
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
