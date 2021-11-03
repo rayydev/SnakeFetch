46b95462169cf626b4e17b228c2594eebd4abf6a
Aleksander <65820898+rayydev@users.noreply.github.com>
2021-11-03 17:34:11 +0100

    Added Files

---
+++ README.md
@@ -0,0 +1,7 @@
+#SnakeFetch
+
+SnakeFetch is like Neofetch or Screen Fetch but written in Python
+
+Refereces:
+Windows ASCII Art - https://textart.io/art/5EvMnhPe5xmN_8ENX11mvweF/microsoft-windows-logo
+Tux ASCII Art - http://www.ascii-art.de/ascii/jkl/linux.txt
--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ import colorama
 import psutil
 import platform
 import socket
-
+from logos import LinuxLogo, WindowsLogo
 uname = platform.uname()
 
+def ShowLogo():
+    LinuxLogo()
+    if os.name in ('nt', 'dos'):
+        WindowsLogo()
+
+ShowLogo()
+print("")
 print(f"System: {uname.system}")
 print(f"Node Name: {uname.node}")
 print(f"Release: {uname.release}")
